cb511a4d4bb834a925d6d7c648937f478370a31e
高橋 郁也 <fumirou0525@takahashi-no-MacBook-Pro.local>
2022-11-10 23:03:23 +0900

    first commit

---
+++ test.js
@@ -0,0 +1,14 @@
+document.getElementById("btn").addEventListener("click", main);
+
+function main(){
+    /*
+    let th_list = document.querySelectorAll('th[bgcolor="#6699CC"]');
+    for (let i = 0; i < th_list.length; i++) {
+        alert(th_list[i].innerText);
+    }    
+    */
+   let a = new Array;
+   a.push(4);
+   a.push(5);
+   alert(a);
+}
