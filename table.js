d564edf1c24ea0fc088585f1ee5d1c01b6335e0c
fumirou05 <fumirou0525@gmail.com>
2022-12-19 19:13:26 +0900

    options.htmlで初期値を表示 (#7)
    * shorten code!
    
    * set initial value to the option page, change setting code of options.js
    
    Co-authored-by: fumirou05 <takahashi.f.ai.titech@gmail.com>

--- table.js
+++ table.js
@@ -1,6 +1,7 @@
 window.onload = () => {
     const columns = ["", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J"];
     const rows = ["", "1", "2", "3", "4", "5", "6", "7"];
+    const mTable = document.getElementById("mtable");
 
     let tmpRow;
     let tmpCell;
@@ -26,6 +27,28 @@ window.onload = () => {
             }
             tmpRow.appendChild(tmpCell);
         }
-        table.appendChild(tmpRow);
+        mTable.appendChild(tmpRow);
     }
+
+    // 設定されている値を表示
+    chrome.storage.sync.get(null, (result) => {
+        console.log(result);
+        let index;
+        let strIndex;
+        let dig1, dig2;
+        if (result.matrix) {
+            for (let row of rows) {
+                for (let column of columns){
+                    if (row && column) {
+                        strIndex = column + row;
+                        dig1 = column.charCodeAt(0) - 64;
+                        dig2 = row.charCodeAt(0) - 48;
+                        index = (dig2 - 1)*10 + dig1 - 1;
+                        document.getElementById(strIndex).value = result.matrix[index];
+                    }
+                }
+            }
+        }
+    })
+
 }
