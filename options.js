5dc6b9d9852c4d1967d76a086388bb4a8a7846a5
fumirou05 <takahashi.f.ai.titech@gmail.com>
2023-01-03 15:42:17 +0900

    organized directory for webpack

--- options.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("btn").addEventListener("click", async () => {
-    let mlist = document.querySelectorAll("#mtable td input");
-    let mvalue = new Array(mlist.length);
-    for (let i = 0; i < mlist.length; i++) {
-        mvalue[i] = mlist[i].value;
-    }
-    chrome.storage.sync.set({ matrix: mvalue });
-    document.getElementById("msg").innerText = "マトリクスコードが設定されました";
-});
